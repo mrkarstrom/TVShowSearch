03da8f5aa850b940d5792cfbd68a681842bbb692
Magnus Karström <83014443+mrkarstrom@users.noreply.github.com>
2021-12-14 19:54:51 +0100

    Done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
-    const config = { params: q: searchTerm}
+    const config = { params: {q: searchTerm} }
     const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
     makeImages(res.data);
     forms.elements.query.value = '';
